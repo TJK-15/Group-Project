f1c6bbe305ad9d7ea18f85727e55b0f772566a40
nrkressin123 <20240977@novaims.unl.pt>
2025-02-14 13:07:32 +0000

    Updates to api.py (not working as of yet)

--- api.py
+++ api.py
@@ -1,43 +1,57 @@
 from flask import Flask, render_template, request, jsonify
-from main import fetch_flickr_photos, fetch_mapillary_photos, save_photos_to_db
+from dotenv import load_dotenv
+import os
+import psycopg2
 
+# initialize Flask as app
 app = Flask(__name__)
+
+# Load .env file
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+# Connect to PostgreSQL
+def get_db_connection():
+    return psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
 
 # flask connects to html file using render_template
 @app.route('/')
 def index():
     return render_template('map.html')  # Make sure map.html is in a "templates" folder
 
+# This endpoint retrieves the latitude, longitude, and radius for the database image request
 @app.route('/api/coordinates', methods=['POST'])
 def get_coordinates():
     data = request.get_json()
     lat = data.get('latitude')
     lng = data.get('longitude')
     radius = data.get('radius')
-    print("Received data:", data)
+    print("Received coords and radius:", data)
 
     if lat is None or lng is None or radius is None:
         return jsonify({'error': 'Invalid input'}), 400
     
-    # Convert radius from str to int, then from meters to kilometers (Flickr uses kilometers)
-    radius = int(radius)
-    radius_km = radius / 1000
-
-    # Fetch photos from Flickr and Mapillary
-    flickr_photos = fetch_flickr_photos(lat, lng, radius_km)
-    #mapillary_photos = fetch_mapillary_photos()
-    #all_photos = flickr_photos + mapillary_photos
-
-    # Save photos to the database
-    save_photos_to_db(flickr_photos)
-
-    # Return the photos in the response
+    # Return the json with coords in the response
     return jsonify({
         'latitude': lat,
         'longitude': lng,
         'radius': radius,
-        'photos': flickr_photos
     })
+    
+    # Searches database for images within radius
+    
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
