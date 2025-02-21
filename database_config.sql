530252c913e5e9468fda9c20351edd0588a98802
Ting-Jia Guo <61928977+TJK-15@users.noreply.github.com>
2025-02-21 18:06:23 +0000

    Update database_config.sql

--- database_config.sql
+++ database_config.sql
@@ -9,7 +9,7 @@ CREATE TABLE locations (
     city TEXT
 );
 
--- Create unique index to ensure the same latitude and longitude not repeated
+-- Create unique index to ensure latitude and longitude sets are unique
 CREATE UNIQUE INDEX unique_location ON locations (latitude, longitude);
 
 -- create owners table
@@ -20,8 +20,8 @@ CREATE TABLE owners (
     repo_id TEXT UNIQUE
 );
 
--- Ensure username and profile_url are unique
-CREATE UNIQUE INDEX unique_owner ON owners (username, profile_url);
+-- Ensure repo_id is unique
+CREATE UNIQUE INDEX unique_owner ON owners (repo_id);
 
 -- create photos table
 CREATE TABLE photos (
@@ -40,9 +40,8 @@ CREATE TABLE photos (
     repo_id TEXT
 );
 
--- Ensure photo uniqueness (title, repo_id, url be unique)
-CREATE UNIQUE INDEX unique_repo_url ON photos (repo_id, url);
-CREATE UNIQUE INDEX unique_photo ON photos (title, url);
+-- Ensure photo url is unique
+CREATE UNIQUE INDEX unique_photo ON photos (url);
 
 -- location_id in photos table should correspond to the id in the locations table
 UPDATE photos
